25d1ec0f7f60435a6ef8a9b0d0197a7029fb8be1
Samuel Gélineau <gelisam+github@gmail.com>
2025-12-17 00:00:50 -0500

    restore not-yet-used token conversion functions

--- src/tokens.ts
+++ src/tokens.ts
@@ -13,25 +13,48 @@ for (let i = 0; i < TOKENS.length; i++) {
   TOKEN_STRING_TO_INDEX[TOKENS[i]] = i;
 }
 
+function indexToTokenNumber(index: number): number {
+  return index + 1;
+}
+
 function indexToShortTokenString(index: number): string {
   return SHORT_TOKENS[index];
 }
+
+function indexToTokenString(index: number): string {
+  return TOKENS[index];
+}
+
 function tokenNumberToIndex(tokenNum: number): number {
   return tokenNum - 1;
 }
-function tokenStringToTokenNumber(token: string): number {
-  return TOKEN_STRING_TO_INDEX[token] + 1;
+
+function tokenNumberToShortTokenString(tokenNum: number): string {
+  return SHORT_TOKENS[tokenNum - 1];
 }
+
 function tokenNumberToTokenString(tokenNum: number): string {
   return TOKENS[tokenNum - 1];
+}
+
+function tokenStringToIndex(token: string): number {
+  return TOKEN_STRING_TO_INDEX[token];
+}
+
+function tokenStringToTokenNumber(token: string): number {
+  return TOKEN_STRING_TO_INDEX[token] + 1;
 }
 
 export {
   NUMBERS,
   LETTERS,
   TOKENS,
+  indexToTokenNumber,
   indexToShortTokenString,
+  indexToTokenString,
   tokenNumberToIndex,
+  tokenNumberToShortTokenString,
+  tokenNumberToTokenString,
+  tokenStringToIndex,
   tokenStringToTokenNumber,
-  tokenNumberToTokenString
 };
